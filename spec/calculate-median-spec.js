f1386308bae7267431c3c7576c4dbab9a2b6a69a
Dann <dann_summer@163.com>
2016-06-02 13:41:29 +0800

    the third work

--- spec/calculate-median-spec.js
+++ spec/calculate-median-spec.js
@@ -2,7 +2,7 @@ var calculate_median = require('../src/calculate-median.js');
 
 describe('calculate_median_spec', function() {
   var collection_a = [1, 2, 3, 4, 5, 6];
-  var collection_b = [6, 2, 3, 8, 5, 1, 7, 4, 9];
+  var collection_b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   it('集合中第偶数个元素的个数为奇数时，计算第偶数个元素的中位数', function() {
     var result = calculate_median(collection_a);
